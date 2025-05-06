0bb81b6f353c4167ef58aa41c42182b071c3d5f9
Smrita Dubey <smritadubey789@gmail.com>
2025-05-06 20:54:35 +0530

    created contactus page

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for contacting us! We’ll get back to you soon.');
+  });
+  
